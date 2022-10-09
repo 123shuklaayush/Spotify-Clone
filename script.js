1fb536011a99b6481eda04ef8471ca38426ef897
Ayush Shukla <123shuklaayush@gmail.com>
2022-10-09 10:51:03 +0530

    Updated music player, pause and continue

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 
 let songIndex = 0;
-let audioElement = new Audio('1.mp3')
+let audioElement = new Audio("songs/1.mp3");
+let masterPlay = document.getElementById('masterplay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+
+// Songs Name
 let songs = [
-    {songName: "Rap God", filePath:'songs/1.mp3'}
+    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
 ]
+
+// Listen to Events
+// ----MY First Song---- //
+masterPlay.addEventListener('click', ()=>{
+
+    // Play and Pause Buttons 
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity =1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.add('fa-play-circle');
+        masterPlay.classList.remove('fa-pause-circle');
+        gif.style.opacity =0;
+    }
+})
+
+// Time Update
+audioElement.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate');
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100)
+    console.log(progress);
+    myProgressBar.value = progress;
+})
