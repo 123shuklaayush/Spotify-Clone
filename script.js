44eba1f095fa53b88f8331bd4d8ceec042d9436a
Ayush Shukla <123shuklaayush@gmail.com>
2022-10-10 18:46:19 +0530

    Updated progressbar

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 let songIndex = 0;
 let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.getElementById('masterplay');
-let myProgressBar = document.getElementById('myProgressBar');
+let myProgressbar = document.getElementById('myProgressbar');
 let gif = document.getElementById('gif');
 
 // Songs Name
 let songs = [
     {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
-    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
-    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
-    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
-    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
-    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
-    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
-    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
-    {songName: "Punji", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Punji", filePath:"songs/2.mp3", coverPath:"covers/2.jpg"},
+    {songName: "Punji", filePath:"songs/3.mp3", coverPath:"covers/3.jpg"},
+    {songName: "Punji", filePath:"songs/4.mp3", coverPath:"covers/4.jpg"},
+    {songName: "Punji", filePath:"songs/5.mp3", coverPath:"covers/5.jpg"},
+    {songName: "Punji", filePath:"songs/6.mp3", coverPath:"covers/6.jpg"},
+    {songName: "Punji", filePath:"songs/7.mp3", coverPath:"covers/7.jpg"},
+    {songName: "Punji", filePath:"songs/8.mp3", coverPath:"covers/8.jpg"},
+    {songName: "Punji", filePath:"songs/9.mp3", coverPath:"covers/9.jpg"},
 ]
 
 // Listen to Events
@@ -39,8 +39,13 @@ masterPlay.addEventListener('click', ()=>{
 
 // Time Update
 audioElement.addEventListener('timeupdate', ()=>{
-    console.log('timeupdate');
     progress = parseInt((audioElement.currentTime/audioElement.duration)*100)
-    console.log(progress);
-    myProgressBar.value = progress;
+    // Formula change percent = (current Time/total Duration)*100 
+
+    myProgressbar.value = progress;
+})
+
+myProgressbar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressbar.value *audioElement.duration/100;
+
 })
